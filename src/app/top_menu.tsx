2711f9041b01f3b5c9385495215f5368f2d8ce7a
MathieuLaureti <mathieu.laureti@gmail.com>
2025-10-18 02:09:02 +0000

    Deploy static build

--- src/app/top_menu.tsx
+++ src/app/top_menu.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function TopMenu() {
   return (
     <header className="sticky top-0 left-0 w-full h-20 bg-gray-900 text-white z-50 shadow">
       <nav className="max-w-screen-xl mx-auto flex items-center justify-between h-full px-4 ">
-        {/* Left: Logo as image */}
-        <a href="/" className="flex items-center">
+        {/* Left: Logo as link */}
+        <Link href="/" className="flex items-center">
           <Image src="placeholders/64x64.svg" alt="Logo" width={48} height={48} />
-        </a>
+        </Link>
 
         {/* Right: Menu buttons */}
         <div className="flex space-x-4">
-          <a href="/schedule" className="px-4 py-2 bg-blue-600 rounded-full hover:bg-blue-700 cursor-pointer transition-transform duration-100 ease-in-out hover:scale-95">
+          <Link
+            href="/schedule"
+            className="px-4 py-2 bg-blue-600 rounded-full hover:bg-blue-700 cursor-pointer transition-transform duration-100 ease-in-out hover:scale-95"
+          >
             Programmation
-          </a>
+          </Link>
           <button
             type="button"
             aria-label="Open menu"
